8514299d7ef3317d220cca17ef3b3ed0c102dc02
IaM DEV <30754770+iam-dev@users.noreply.github.com>
2025-06-05 12:38:51 +0200

    Update wallet integration to @txnlab/use-wallet-react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { WalletProvider, PROVIDER_ID, NetworkId } from '@txnlab/use-wallet-react';
+import { DeflyWalletConnect } from '@blockshake/defly-connect';
+import { PeraWalletConnect } from '@perawallet/connect';
 import App from './App.tsx';
 import './index.css';
 
+const walletProviders = {
+  [PROVIDER_ID.DEFLY]: {
+    id: PROVIDER_ID.DEFLY,
+    name: 'Defly Wallet',
+    connect: async () => new DeflyWalletConnect(),
+  },
+  [PROVIDER_ID.PERA]: {
+    id: PROVIDER_ID.PERA,
+    name: 'Pera Wallet',
+    connect: async () => new PeraWalletConnect(),
+  },
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WalletProvider
+      value={{
+        providers: walletProviders,
+        defaultActiveNetwork: NetworkId.TestNet,
+      }}
+    >
+      <App />
+    </WalletProvider>
   </StrictMode>
 );
